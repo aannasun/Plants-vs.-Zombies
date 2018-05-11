6e57b8008cfed4ac351d6823050a7a9f53800727
elainewcai <ca.elaine.cai@gmail.com>
2018-05-11 13:57:32 -0700

    Update Sunflower.java

--- src/Sunflower.java
+++ src/Sunflower.java
@@ -1,3 +1,31 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Sunflower extends Plants{
 	
+	private int xCol, yRow;
+	private BufferedImage img;
+	{
+		try 
+		{
+			img = ImageIO.read(new File("sunflower.png"));
+		}
+
+		catch(IOException e) {
+
+		}
+	}
+	
+	public Sunflower(int x, int y) {
+		xCol = x;
+		yRow = y;
+	}
+
+	public void draw(Graphics g) {
+		g.drawImage(img, xCol, yRow, null);
+	}
 }
