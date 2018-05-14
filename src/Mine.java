be2973aab871cdf943cbd9c9e11fd598227f26a0
elainewcai <ca.elaine.cai@gmail.com>
2018-05-13 17:45:39 -0700

    Update Mine.java

--- src/Mine.java
+++ src/Mine.java
@@ -1,3 +1,31 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Mine extends Plants{
 	
+	private int xCol, yRow;
+	private BufferedImage img;
+	{
+		try 
+		{
+			img = ImageIO.read(new File("mine.png"));
+		}
+
+		catch(IOException e) {
+
+		}
+	}
+	
+	public Mine(int x, int y) {
+		xCol = x;
+		yRow = y;
+	}
+
+	public void draw(Graphics g) {
+		g.drawImage(img, xCol, yRow, null);
+	}
 }
