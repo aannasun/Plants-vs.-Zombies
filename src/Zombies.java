e9f953a308e2e89702c4e5844842512f21c6b6a8
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-05-14 21:01:41 +0000

    Update Zombies.java

--- src/Zombies.java
+++ src/Zombies.java
@@ -1,17 +1,36 @@
-abstract class Zombies {
-	
-	int health = 30;
-	
-	public int getHealth() {
-	  return health;
-  	}
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class Zombies {
+
+	private static int damage = 20;
+	private int xCol, yRow;
+	private BufferedImage img;
+	{
+		try 
+		{
+			img = ImageIO.read(new File("zombie.png"));
+		}
+
+		catch(IOException e) {
+
+		}
+	}
+	public Zombies() {
+		xCol = (9*80);
+		yRow = (int)(Math.random()*4*100);
+	}
 	
 	public void draw(Graphics g) {
-	
-  	}
+		g.drawImage(img, xCol, yRow, null);
+	}
 	
 	public void move() {
-	
+		xCol-=5;
 	}
-		
 }
