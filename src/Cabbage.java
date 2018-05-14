50e540758173f6442875342e62bb46ef3f88eb52
elainewcai <ca.elaine.cai@gmail.com>
2018-05-13 17:31:17 -0700

    Update Cabbage.java

--- src/Cabbage.java
+++ src/Cabbage.java
@@ -1,3 +1,31 @@
-public class Cabbage extends Plants{
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class Cabbage extends Plants{
+	
+	private int xCol, yRow;
+	private BufferedImage img;
+	{
+		try 
+		{
+			img = ImageIO.read(new File("cabbage.png"));
+		}
+
+		catch(IOException e) {
+
+		}
+	}
+	
+	public Cabbage(int x, int y) {
+		xCol = x;
+		yRow = y;
+	}
+
+	public void draw(Graphics g) {
+		g.drawImage(img, xCol, yRow, null);
+	}
 }
