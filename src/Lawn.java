22eb1babd7eb01a5c78e85566b9841a26ae9023d
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-05-14 01:12:19 +0000

    Update Lawn.java

--- src/Lawn.java
+++ src/Lawn.java
@@ -14,16 +14,22 @@ public class Lawn {
 	private int x;
 	private int y;
 	private ArrayList<Plants> plants= new ArrayList<Plants>();
+	private ArrayList<Zombies> z = new ArrayList<Zombies>();
+	private ArrayList<Sun> suns = new ArrayList<Sun>();
 	private final int sqSide = 80;
 	private String s;
+	private int numZ = 5;
+	private int points = 50;
+	//card height = 63;
 	//	private Block[][] grid = new Block[5][9];
-	private BufferedImage frontyard, peashooter_card, sunflower_card, walnut_card, cabbage_card, mine_card;
+	private BufferedImage frontyard, sun, sunflower_card, peashooter_card, walnut_card, cabbage_card, mine_card;
 
 	{
 		try {
 			frontyard = ImageIO.read(new File("frontyard.png"));
-			peashooter_card = ImageIO.read(new File("peashooter_card.png"));
+			sun = ImageIO.read(new File("sun.png"));
 			sunflower_card = ImageIO.read(new File("sunflower_card.png"));
+			peashooter_card = ImageIO.read(new File("peashooter_card.png"));
 			walnut_card = ImageIO.read(new File("walnut_card.png"));
 			cabbage_card = ImageIO.read(new File("cabbage_card.png"));
 			mine_card = ImageIO.read(new File("mine_card.png"));
@@ -31,63 +37,122 @@ public class Lawn {
 		catch(IOException e) {
 
 		}
-//		System.out.println(img);
+		//		System.out.println(img);
 	}
 
 	public void draw(Graphics g) {
 		g.drawImage(frontyard, 0, 0, null);
-		g.drawImage(peashooter_card, 720, 0, null);
-		g.drawImage(sunflower_card, 720, 60, null);
-		g.drawImage(walnut_card, 720, 120, null);
-		g.drawImage(cabbage_card, 720, 180, null);
-		g.drawImage(mine_card, 720, 240, null);
+		g.drawImage(sunflower_card, 720, 0, null);
+		g.drawImage(peashooter_card, 720, 63, null);
+		g.drawImage(walnut_card, 720, 126, null);
+		g.drawImage(cabbage_card, 720, 63*3, null);
+		g.drawImage(mine_card, 720, 63*4, null);
 		for(Plants p: plants) {
 			p.draw(g);
 		}
+		for(Zombies zombies: z) {
+			zombies.draw(g);
+		}
+		for(Sun s: suns) {
+			s.draw(g);
+		}
+		g.drawString("" + points, 750, 500-50);
 	}
 
 	public void add(String s, int x, int y) {
-		if(s.equals("peashooter"))
-			plants.add(new Peashooter(x, y));
-		if(s.equals("sunflower"))
-			plants.add(new Sunflower(x, y));
-		if(s.equals("walnut"))
-			plants.add(new Walnut(x, y));
-		if(s.equals("cabbage"))
+		if(s.equals("sunflower")) {
+			if(points >= 50) {
+				plants.add(new Sunflower(x, y));
+				points -= 50;
+			}
+		}
+		else if(s.equals("peashooter")) {
+			if(points >= 100) {
+				plants.add(new Peashooter(x, y));
+				points -=100;
+			}
+		}
+		else if (s.equals("walnut")) {
+			if(points >= 50) {
+				plants.add(new Walnut(x, y));
+				points -= 50;
+			}
+		}
+		else if (s.equals("cabbage")) {
+			if(points >= 100) {
 			plants.add(new Cabbage(x, y));
-		if(s.equals("mine"))
-			plants.add(new Mine(x, y));
-	}
-	
-	
+			points -=100;
+			}
+		}
+		else if(s.equals("mine")) {
+			if(points >= 50) {
+				plants.add(new Mine(x, y));
+				points -=50;
+			}
+		}
+	} 
+
 	public void addStuff() {
-//		String s = JOptionPane.showInputDialog("What plant do you want");
 		add(s, this.x, this.y);
 		s = "";
 	}
-	
+
 	public void justClicked(int x, int y) {
 		this.x = (x/80)*80;
 		this.y = (y/100)*100;
+		for(int i = suns.size()-1; i>-1; i--) {
+			if(suns.get(i).checkIfClicked(x, y)) {
+				suns.remove(i);
+				points +=25;
+			}
+		}
 	}
-	
-	public void peashooterCard() {
-		s = "peashooter";
+
+	public void card(int y) {
+		if(y >= 0 && y <= 63) {
+			s = "sunflower";
+		}
+		else if(y > 63 && y <= 126) {
+			s = "peashooter";
+		}
+		else if(y > 126 && y <= 63*3) {
+			s = "walnut";
+		}
+		else if(y > 63*3 && y <= 63*4) {
+			s = "cabbage";
+		}
+		else if(y > 63*4 && y <= 63*5) {
+			s = "mine";
+		}
 	}
-	
-	public void sunflowerCard() {
-		s = "sunflower";
+
+	public void addZombie() {
+		if(numZ > 0) {
+			z.add(new Zombies());
+			numZ--;
+		}
 	}
-	
-	public void walnutCard() {
-		s = "walnut";
+
+	public void moveZombies() {
+		for(Zombies zombie: z) {
+			zombie.move();
+		}
 	}
-	
-	public void CabbageCard() {
-		s = "cabbage";
+
+	public void dropSun() {
+		suns.add(new Sun());
 	}
-	
-	public void MineCard() {
-		s = "mine";
+
+	public void moveSuns() {
+		for(Sun s: suns) {
+			if(s.getY() <= 500) {
+				s.move();
+			}
+		}
+		for(int i = suns.size()-1; i>-1; i--) {
+			if(suns.get(i).getY()>500) {
+				suns.remove(i);
+			}
+		}
 	}
 }
