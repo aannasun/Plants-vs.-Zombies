6fe9c333ffc4bb47128222f91081d20f2d79abf5
elainewcai <ca.elaine.cai@gmail.com>
2018-05-16 16:31:07 -0700

    Update Peashooter.java

--- src/Peashooter.java
+++ src/Peashooter.java
@@ -28,7 +28,7 @@ public class Peashooter extends Plants{
 	public Peashooter(int x, int y) {
 		xCol = x;
 		yRow = y;
-		p = new Pea(xCol+80, yRow+20);
+		p = new Pea(xCol+100, yRow-30, 0);
 	}
 
 	public void draw(Graphics g) {
@@ -44,15 +44,15 @@ public class Peashooter extends Plants{
 	public String name() {
 		return "peashooter";
 	}
-	
+
 	public void newPea() {
-		p = new Pea(xCol+80, yRow+20);
+		p = new Pea(xCol+80, yRow+20, 0);
 	}
-	
-	
+
+
 	public void shoot() {
-		
+
 		p.changeX();
-		
+
 	}
 }
