a58f917038aa6d5c9f1f201ea34188fb6c56958a
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-05-16 21:20:33 +0000

    Update Peashooter.java

--- src/Peashooter.java
+++ src/Peashooter.java
@@ -9,10 +9,11 @@ import javax.imageio.ImageIO;
 public class Peashooter extends Plants{
 
 	private static int damage = 20;
-//	private static int X_LEN  = 80;
-//	private static int Y_WIDTH = 100;
+	//	private static int X_LEN  = 80;
+	//	private static int Y_WIDTH = 100;
 	private int xCol, yRow;
-	private BufferedImage img;
+	private Pea p;
+	private BufferedImage img, pea1, pea3;
 	{
 		try 
 		{
@@ -27,10 +28,29 @@ public class Peashooter extends Plants{
 	public Peashooter(int x, int y) {
 		xCol = x;
 		yRow = y;
+		p = new Pea(xCol+100, yRow-30);
 	}
 
 	public void draw(Graphics g) {
-		if (health>0)
-			g.drawImage(img, xCol, yRow, null);
+		g.drawImage(img, xCol, yRow, null);
+		if(p.newPea()) {
+			newPea();
+		}
+		p.draw(g);
+	}
+
+	public String name() {
+		return "peashooter";
+	}
+	
+	public void newPea() {
+		p = new Pea(xCol+80, yRow+20);
+	}
+	
+	
+	public void shoot() {
+		
+		p.changeX();
+		
 	}
 }
