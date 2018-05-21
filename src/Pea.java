8a51451b234fd40fafa950179df5d8253104dd7d
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-05-21 20:33:27 +0000

    Update Pea.java

--- src/Pea.java
+++ src/Pea.java
@@ -1,54 +1,41 @@
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Pea{
+public class Pea extends Projectile{
 	private int x;
 	private int y;
-	private int color;
 	private boolean stop = false;
 	private boolean newPea;
-	//size = 28
-	
+	private boolean hit;
 	BufferedImage pea; 
 	{
 		try 
 		{
 			pea = ImageIO.read(new File("pea.png"));
-		}
-		catch(IOException e) {
-		}
-	}
-	BufferedImage bpea; 
-	{
-		try 
-		{
-			bpea = ImageIO.read(new File("bpea.png"));
-		}
-		catch(IOException e) {
-		}
-	}
-	
-	public Pea() {
 		
+			
+		}
+
+		catch(IOException e) {
+
+		}
 	}
 	
-	public Pea(int x, int y, int c) {
+	
+	public Pea(int x, int y) {
 		this.x = x;
 		this.y = y;
-		color = c;
 		newPea = false;
 	}
 	
 	public void draw(Graphics g) {
 		if(!stop) {
-			if (color==0)
-				g.drawImage(pea, x, y, null);
-			if (color==1)
-				g.drawImage(bpea, x, y, null);
+		g.drawImage(pea, x, y, null);
 		}
 		
 		
@@ -69,18 +56,29 @@ public class Pea{
 			
 		}
 	}
-
-	public void ifHit(Zombies z) {
+	
+	public boolean atZombie(Zombies z) {
+//		System.out.println(z.getY());
 		if(z.getX() == x) {
-		System.out.println("hi");
-			if(z.getRow() == (int)(y/80)) {
-				System.out.println("hit");	
+			if(z.getY() <= y && (z.getY()+100 > y)){
+//				stop = true;
+				return true;
 			}
 		}
+		
+		return false;
 	}
-	
-	public int getRow() {
-		return (int)(y/80);
+
+	@Override
+	public Point getCoord() {
+		
+		return new Point(x,y);
 	}
-	
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		stop = true;
+	}
+
 }
