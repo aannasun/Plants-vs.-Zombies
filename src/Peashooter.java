916343b8a5ce3f7628afdaedd171e14ae08e9d60
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-05-21 20:28:08 +0000

    Update Peashooter.java

--- src/Peashooter.java
+++ src/Peashooter.java
@@ -12,12 +12,15 @@ public class Peashooter extends Plants{
 	//	private static int X_LEN  = 80;
 	//	private static int Y_WIDTH = 100;
 	private int xCol, yRow;
+	private boolean toLeft = true;
 	private Pea p;
-	private BufferedImage img, pea1, pea3;
+	private BufferedImage img, left, right;
 	{
 		try 
 		{
 			img = ImageIO.read(new File("peashooter.png"));
+			left = ImageIO.read(new File("peashooter_left.png"));
+			right = ImageIO.read(new File("peashooter_right.png"));
 		}
 
 		catch(IOException e) {
@@ -26,36 +29,60 @@ public class Peashooter extends Plants{
 	}
 
 	public Peashooter(int x, int y) {
+		img = left;
 		xCol = x;
 		yRow = y;
-		p = new Pea(xCol+100, yRow-30, 0);
+		health = 30;
+		p = new Pea(xCol+80, yRow+20);
 	}
 
 	public void draw(Graphics g) {
-		if (health>0) {
-			g.drawImage(img, xCol, yRow, null);
-			if(p.newPea()) {
-				newPea();
-			}
-			p.draw(g);
+		g.drawImage(img, xCol, yRow, null);
+
+		if(p.newPea()) {
+			newPea();
 		}
+		p.draw(g);
 	}
 
+	public void move() {
+		if(toLeft) {
+			img = left;
+			toLeft = false;
+		}
+		else {
+			img = right;
+			toLeft = true;
+		}
+	}
+	
 	public String name() {
 		return "peashooter";
 	}
 
 	public void newPea() {
-		p = new Pea(xCol+80, yRow+20, 0);
+		p = new Pea(xCol+80, yRow+20);
 	}
 
-	public Pea getPea() {
-		return p;
+	public boolean projectileAtZombie(Zombies z) {
+		return p.atZombie(z);
+	}
+
+	public int getX() {
+		return xCol;
 	}
 	
-//	public void shoot() {
-//
-//		p.changeX();
-//
-//	}
+	public int getY() {
+		return yRow;
+	}
+	
+	public void shoot() {	
+		p.changeX();	
+	}
+
+	@Override
+	public Projectile getProjectile() {
+		// TODO Auto-generated method stub
+		return p;
+	}
 }
