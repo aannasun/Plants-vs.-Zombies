fbc057696e5992242ecbc26196ab2c7f8d38551f
elainewcai <ca.elaine.cai@gmail.com>
2018-05-21 13:29:18 -0700

    Update Plants.java

--- src/Plants.java
+++ src/Plants.java
@@ -3,6 +3,8 @@ import java.awt.Graphics;
 abstract class Plants {
 	
 	int health = 30;
+	boolean shoot = true;
+	boolean left = true;
 
 	public void draw(Graphics g) {
 				
@@ -23,5 +25,19 @@ abstract class Plants {
 	public void shoot() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void setRestPosition() {
+		if (left == true)
+			left = false;
+		else if (left == false)
+			left = true;
+	}
+	
+	public void setShootPosition() {
+		if (shoot == true)
+			shoot = false;
+		else if (shoot == false)
+			shoot = true;
 	}
 }
