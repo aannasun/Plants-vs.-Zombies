c1e3e4bcbbc2f1ef6d514da7f73d7f1233de45fe
elainewcai <ca.elaine.cai@gmail.com>
2018-05-22 13:53:30 -0700

    Update SnowPea.java

--- src/SnowPea.java
+++ src/SnowPea.java
@@ -13,11 +13,11 @@ public class SnowPea extends Plants{
 	//	private static int Y_WIDTH = 100;
 	private int xCol, yRow;
 	private Pea p;
-	private BufferedImage img, pea1, pea3;
+	private BufferedImage img1;
 	{
 		try 
 		{
-			img = ImageIO.read(new File("snowpea.png"));
+			img1 = ImageIO.read(new File("snowpea.png"));
 		}
 
 		catch(IOException e) {
@@ -26,19 +26,30 @@ public class SnowPea extends Plants{
 	}
 
 	public SnowPea(int x, int y) {
+		//		super(x, y);
+		//img = left;
 		xCol = x;
 		yRow = y;
-		p = new Pea(xCol+100, yRow-30, 1);
+		health = 30;
+		p = new Pea(xCol+80, yRow+20, 0);
 	}
 
 	public void draw(Graphics g) {
 		if (health>0) {
-			g.drawImage(img, xCol, yRow, null);
+			g.drawImage(img1, xCol, yRow, null);
+			/*if (shoot==true)
+				g.drawImage(img1, xCol, yRow, null);
+			if (shoot==false)
+				g.drawImage(img2, xCol, yRow, null);*/
 			if(p.newPea()) {
 				newPea();
 			}
 			p.draw(g);
 		}
+	}
+
+	public int getRow() {
+		return (int)((yRow-80)/90);
 	}
 
 	public String name() {
@@ -49,13 +60,25 @@ public class SnowPea extends Plants{
 		p = new Pea(xCol+80, yRow+20, 1);
 	}
 
-	public Pea getPea() {
+	public boolean projectileAtZombie(Zombies z) {
+		return p.atZombie(z);
+	}
+
+	public int getX() {
+		return xCol;
+	}
+
+	public int getY() {
+		return yRow;
+	}
+
+	public void shoot() {	
+		p.changeX();	
+	}
+
+	@Override
+	public Projectile getProjectile() {
+		// TODO Auto-generated method stub
 		return p;
 	}
-	
-//	public void shoot() {
-//
-//		p.changeX();
-//
-//	}
 }
