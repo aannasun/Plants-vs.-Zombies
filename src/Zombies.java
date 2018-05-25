807c5d8c9d34c4ba5d586f1c3ab345a876309fc6
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-05-25 21:03:30 +0000

    Update Zombies.java

--- src/Zombies.java
+++ src/Zombies.java
@@ -11,12 +11,15 @@ public class Zombies {
 	private int health = 30;
 	private static int damage = 20;
 	private int xCol, yRow;
-	private BufferedImage img, broken;
+	private BufferedImage img, broken, walkingleft, walkingright;
 	private int twoSeconds = 0;
+	private boolean left = true;
 	{
 		try 
 		{
 			img = ImageIO.read(new File("zombie.png"));
+			walkingleft = ImageIO.read(new File("z_left.png"));
+			walkingright = ImageIO.read(new File("z_right.png"));
 		}
 
 		catch(IOException e) {
@@ -24,29 +27,48 @@ public class Zombies {
 		}
 	}
 	public Zombies() {
+//		xCol = (9*80);
 		xCol =  850;
+//		yRow = (int)(Math.random()*5)*100;
 		yRow = (int)(Math.random()*5)*90+80;
 	}
 	
 	public Zombies(int row) {
+//		xCol = (9*80);
+//		yRow = row*100;
 		xCol = 850;
 		yRow = row*90+80;
 	}
 	
 	public void draw(Graphics g) {
 		if(health > 0) {
-			g.drawImage(img, xCol, yRow, null);
-		}
-		else {
+			if(left) {
+				g.drawImage(walkingleft, xCol, yRow, null);
+			}
+			else {
+				g.drawImage(walkingright, xCol, yRow, null);
+			}
+//			g.drawImage(img, xCol, yRow, null);
 		}
 	}
 	
-
+//	public void stopMoving() {
+//		xCol = xCol;
+//	}
+//	
 	public void move() {
 		xCol-=5;
+		if(left) {
+			left = false;
+		}
+		else {
+			left = true;
+		}
 	}
 	
 	public boolean checkIfAtPlant(Plants p) {
+//		System.out.println("Z:" + xCol + " " + yRow/100);
+//		System.out.println("P:" + p.getX() + " " + p.getY()/100);
 		if((int)(yRow/100) == (int)(p.getY()/100)) {
 			if(p.getX()+70 == xCol) {
 				return true;
@@ -64,10 +86,12 @@ public class Zombies {
 	}
 	
 	public int getCol() {
+//		return (int)(xCol/80);
 		return (int) ((xCol-145)/75);
 	}
 	
 	public int getRow() {
+//		return (int)(yRow/100);
 		return (int)((yRow-80)/90);
 	}
 	
