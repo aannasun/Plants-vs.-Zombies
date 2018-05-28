be16fab063fa4ee37ebe059cec3bd8c190ebd1c7
aannasun <39067485+aannasun@users.noreply.github.com>
2018-05-28 12:27:32 -0700

    Update Corn.java

--- src/Corn.java
+++ src/Corn.java
@@ -1,3 +1,15 @@
 public class Corn extends Plants{
 	
+	@Override
+	public Projectile getProjectile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isProjectile() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
 }
