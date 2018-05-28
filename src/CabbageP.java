b703059840b430a74426e04729fb4bdd6877bc3f
aannasun <39067485+aannasun@users.noreply.github.com>
2018-05-28 12:25:28 -0700

    Create CabbageP.java

---
+++ src/CabbageP.java
@@ -0,0 +1,23 @@
+import java.awt.Point;
+
+public class CabbageP extends Projectile{
+
+	@Override
+	public Point getCoord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void go() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
