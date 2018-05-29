98c0003731a6667c583231d8a11a1bb0174f47ef
aannasun <39067485+aannasun@users.noreply.github.com>
2018-05-29 13:21:03 -0700

    Update Cabbage.java

--- src/Cabbage.java
+++ src/Cabbage.java
@@ -1,14 +1,17 @@
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Cabbage extends Plants{
-	private CabbageP cabbageP;
+public class Cabbage extends Plants {
+
 	private int xCol, yRow;
-	private int health = 100;
+	private CabbageP cabbageP;
+	private int health = 30;
+	private int zx, zy;
 	private BufferedImage img;
 	{
 		try 
@@ -20,26 +23,63 @@ public class Cabbage extends Plants{
 
 		}
 	}
-	
+
 	public Cabbage(int x, int y) {
 		xCol = x;
 		yRow = y;
+		cabbageP = new CabbageP(xCol+80, yRow-200, zx, zy);
 	}
 
 	public void draw(Graphics g) {
-		if (health>0)
+		if (health>0) {
 			g.drawImage(img, xCol, yRow, null);
+			
+				if(cabbageP.newCabbage()) {
+					newCabbage();
+				}
+				cabbageP.draw(g);
+			}
 	}
-	
-		@Override
-	public Projectile getProjectile() {
-		// TODO Auto-generated method stub
-		return cabbageP;
+
+	private void newCabbage() {
+		cabbageP = new CabbageP(xCol+80, yRow-200, zx, zy);
+	}
+
+	public String name() {
+		return "cabbage";
+	}
+
+	public void shoot() {
+		cabbageP.shoot();
 	}
 
 	@Override
+	public Projectile getProjectile() {
+		return cabbageP;
+	}
+
+
+	@Override
 	public boolean isProjectile() {
-		// TODO Auto-generated method stub
 		return true;
 	}
+
+	public boolean isProjMotion() {
+		return true;
+		
+	}
+
+	public void zombCoord(int x, int y) {
+		zx = x;
+		zy = y;
+	}
+
+	public boolean projectileAtZombie(Zombies z) {
+		return cabbageP.atZombie(z);
+	}
+	
+	public int getRow() {
+		return (int)((yRow-80)/90);
+	}
 }
+
