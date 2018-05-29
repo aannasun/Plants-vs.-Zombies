5d9eeb106fd1f3e4bcaeb5d38288a327e222cbf0
elainewcai <ca.elaine.cai@gmail.com>
2018-05-29 13:12:47 -0700

    Update Mine.java

--- src/Mine.java
+++ src/Mine.java
@@ -8,11 +8,13 @@ import javax.imageio.ImageIO;
 public class Mine extends Plants{
 	
 	private int xCol, yRow;
-	private BufferedImage img;
+	//private int time = 0;
+	private BufferedImage img, top;
 	{
 		try 
 		{
 			img = ImageIO.read(new File("mine.png"));
+			top = ImageIO.read(new File("minetop.png"));
 		}
 
 		catch(IOException e) {
@@ -23,10 +25,40 @@ public class Mine extends Plants{
 	public Mine(int x, int y) {
 		xCol = x;
 		yRow = y;
+		health = 20;
 	}
 
 	public void draw(Graphics g) {
-		if (health>0)
+		if (time < 10)
+			g.drawImage(top, xCol, yRow, null);
+		else
 			g.drawImage(img, xCol, yRow, null);
+		
+	}
+
+	
+	public void decreaseHealth(int h) {
+		health -= h;
+	}
+	
+	public int getY() {
+		return yRow;
+	}
+	public int getX() {
+		return xCol;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+
+	@Override
+	public Projectile getProjectile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String name() {
+		return "mine";
 	}
 }
