fbb573d3b4948afe4e2404fa0f8fddc14d40df88
kathyz16 <39105999+kathyz16@users.noreply.github.com>
2018-06-01 07:24:53 +0000

    Update Bokchoi.java

--- src/Bokchoi.java
+++ src/Bokchoi.java
@@ -7,53 +7,92 @@ import javax.imageio.ImageIO;
 
 public class Bokchoi extends Plants{
 	
-	
-	private int xCol, yRow;
-	private BufferedImage left, right, img1, img2;
-	{
-		try 
-		{
-			right = ImageIO.read(new File("bokchoi1.png"));
-			left = ImageIO.read(new File("bokchoi2.png"));
-			img1 = ImageIO.read(new File("bokfight1.png"));
-			img2 = ImageIO.read(new File("bokfight2.png"));
+	private int zx;
+	private boolean atZombie = false;
+	private BufferedImage img, fight2;
+	{ 
+		try {
+			img = ImageIO.read((getClass().getResource("bokchoi1.png")));
+			fight2 = ImageIO.read((getClass().getResource("bokfight2.png")));
+//			img = ImageIO.read(new File("bokchoi1.png"));
+//			fight2 = ImageIO.read(new File("bokfight2.png"));
 		}
-
 		catch(IOException e) {
-
+			
 		}
 	}
 	
 	public Bokchoi(int x, int y) {
-		xCol = x;
-		yRow = y;
-		health = 20;
+		setX(x);
+		setY(y);
+		setHealth(100);
 	}
-
+	
 	public void draw(Graphics g) {
-		if (health>0) {
-			if (onZombies==true) {
-				if (shoot==true)
-					g.drawImage(img1, xCol, yRow, null);
-				if (shoot==false)
-					g.drawImage(img2, xCol, yRow, null);
-			}
+		if(getHealth() > 0) {
+			if(!atZombie) {
+					g.drawImage(img, getX(), getY(), null);
+				}
 			else {
-				if (left_==true)
-					g.drawImage(left, xCol, yRow, null);
-				if (left_==false)
-					g.drawImage(right, xCol, yRow, null);
+				g.drawImage(fight2, getX(), getY(), null);
 			}
 		}
 	}
+	
+	//moves the bokchoi
+	public void move() {
+		if(atZombie) {
+			atZombie = false;
+		}
+		else {
+			atZombie = true;
+		}
+	}
+	
+	//returns false because there is no projectile
+	public boolean isProjectile() {
+		return false;
+	}
+	
+	//returns true because there is "motion"
+	public boolean isProjMotion() {
+		return true;
+	}
+	
+	//punches
+	public void shoot() {
+		if(zx >= getX()-73 && zx <= getX()+73) {
+			atZombie = true;
+		}
+	}
+	
+	//sets zombie coordinate
+	public void zombCoord(int x, int y) {
+		zx = x;
+	}
 
-	@Override
+	//returns null because no projectile
 	public Projectile getProjectile() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	//returns which column it is in
+	public int getCol() {
+		return (int) ((getX()-145)/75);
+	}
+	
+	//returns which row it is in
+	public int getRow() {
+		return (int)((getY()-80)/90);
+	}
+	
+	//name
 	public String name() {
 		return "bokchoi";
+	}
+	
+	//returns is the projectile is at the zombie
+	public boolean projectileAtZombie(Zombies z) {
+		return atZombie;
 	}
 }
